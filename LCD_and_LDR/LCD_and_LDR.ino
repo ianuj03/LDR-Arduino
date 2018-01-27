674a0c6f55614d19b0c5424641c8224a0ac5ec21
Anuj Jain <33128728+ianuj03@users.noreply.github.com>
2018-01-27 17:17:39 +0530

    Update LCD_and_LDR.ino

--- LCD_and_LDR/LCD_and_LDR.ino
+++ LCD_and_LDR/LCD_and_LDR.ino
@@ -6,23 +6,23 @@
 #include<LiquidCrystal.h>
 LiquidCrystal lcd(2,3,4,5,6,7);
 const int LDR=A0;
-const int led=8;
+//const int led=8;
 float ldr_value,total_value,lux;
 void setup(){
   pinMode(LDR,INPUT);
-  pinMode(led,OUTPUT);
-  lcd.begin(16,2);
+  //pinMode(led,OUTPUT);
+  lcd.begin(16,2);//Column,row
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Intensity:");
 }
 void loop(){
   ldr_value=analogRead(LDR);
   total_value=ldr_value*0.0048828125;
   lux=(2500/total_value-500)/10;
-  lcd.setCursor(0,0);
-  lcd.print("Intensity:");
   lcd.setCursor(0,1);
   lcd.print(lux);
   lcd.print(" lumens.");
-  analogWrite(led,lux);
+  //analogWrite(led,lux);
   delay(500);
-  //digitalWrite(led,0);
 }
